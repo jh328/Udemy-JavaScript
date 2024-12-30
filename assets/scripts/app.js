27638c411b4685108f6cecb0fdbfd58e26189618
jh328 <wnsgks32810@naver.com>
2024-12-31 08:07:23 +0900

    section 04 stat

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,62 +2,67 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
-function getUserInput() {
-  return parseInt(userInput.value);
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
 }
 
-function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${operator} ${resultBeforeCalc} + ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor file
 }
 
 function writeToLog(
   operationIdentifier,
-  prevReesult,
+  prevResult,
   operationNumber,
   newResult
 ) {
   const logEntry = {
     operation: operationIdentifier,
-    prevResult: prevReesult,
+    prevResult: prevResult,
     number: operationNumber,
     result: newResult,
   };
   logEntries.push(logEntry);
-  console.log("logEntries.push : ", logEntries);
+  console.log(logEntries);
+}
+
+function calculateResult(calculationType) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === "ADD") {
+    currentResult += enteredNumber;
+    mathOperator = "+";
+  } else if (calculateResult === "SUBTRACT") {
+    currentResult -= enteredNumber;
+    mathOperator = "-";
+  } else if (calculateResult === "MULTIPLY") {
+    currentResult *= enteredNumber;
+    mathOperator = "*";
+  } else if (calculateResult === "DIVIDE") {
+    currentResult /= enteredNumber;
+    mathOperator = "/";
+  }
 }
 
 function add() {
-  const enteredNumber = getUserInput();
-  const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteLog("+", initialResult, enteredNumber);
-  writeToLog("ADD", initialResult, enteredNumber, currentResult);
+  calculateResult("ADD")
 }
 
-// 뺄셈
 function subtract() {
-  const enteredNumber = getUserInput();
-  const initialResult = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteLog("-", initialResult, enteredNumber);
-  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+  calculateResult("SUBTRACT")
+
 }
-// 곱셈
+
 function multiply() {
-  const enteredNumber = getUserInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteLog("*", initialResult, enteredNumber);
-  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+  calculateResult("MULTIPLY")
+
 }
-//나눗셈
+
 function divide() {
-  const enteredNumber = getUserInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteLog("/", initialResult, enteredNumber);
-  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+  calculateResult("DIVIDE")
+
 }
 
 addBtn.addEventListener("click", add);
